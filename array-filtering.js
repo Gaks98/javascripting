47575271e190d7650ebdd6b6331c4c4f44c2a43d
Gaks98 <yvonnegax98@gmail.com>
2020-07-05 21:39:53 +0300

    add array-filtering.js

---
+++ array-filtering.js
@@ -0,0 +1,29 @@
+/*
+* There are many ways to manipulate arrays.
+
+* One common task is filtering arrays to   only contain certain values.
+
+* For this we can use the.filter() method.
+
+*Here is an example:
+
+  const pets = ['cat', 'dog', 'elephant']
+
+  const filtered = pets.filter(function (pet) {
+    return (pet !== 'elephant')
+  })
+
+*The filtered variable will now only contain cat and dog.
+*/
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// const filtered = numbers.filter(function (number){
+//   return number % 2 === 0
+// })
+
+const filtered = numbers.filter(function evenNumbers(number){
+  return number % 2 === 0
+});
+
+console.log(filtered);
